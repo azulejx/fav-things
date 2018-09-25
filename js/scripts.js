4d289a119a8a32215ebc96ae241b564b23091278
Azul Rodriguez Da Silva and richard laws <richard@>
2018-09-24 20:27:24 -0700

    add business and user logic in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,45 @@
-alert("Hi");
+var thing1 = "";
+var thing2 = "";
+var thing3 = "";
+var thing4 = "";
+var allthings = [];
+var final = [];
+
+var finalArray = function() {
+  final.push(allthings[1], allthings[0], allthings[2])
+};
+
+$(document).ready(function() {
+  $("form#favform").submit(function(event) {
+    event.preventDefault();
+
+    thing1 = $("#favthing1").val();
+    thing2 = $("#favthing2").val();
+    thing3 = $("#favthing3").val();
+    thing4 = $("#favthing4").val();
+
+    allthings.push(thing1);
+    allthings.push(thing2);
+    allthings.push(thing3);
+    allthings.push(thing4);
+
+    finalArray();
+
+    $("#fourth").text(final);
+    // console.log(allthings);
+    //
+    // $("#first").text(allthings);
+
+    // var empty = [];
+    // var final = empty.push(thing2,thing1,thing3);
+    // var print = final.toString();
+
+    // var final = .push(allthings[1],allthings[0],allthings[2]);
+
+    // $("#first").text(allthings[1]);
+    // $("#second").text(allthings[0]);
+    // $("#third").text(allthings[2]);
+    // $("#fourth").text(print);
+
+  });
+});
